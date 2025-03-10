54e2d712013efcb72b444db463fe4de2c4455fd9
palak garg <gargpalak0401@gmail.com>
2025-03-11 01:11:06 +0530

    tic-tac-toe

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,3 +1,28 @@
-export default function Square() {
-  return <button className="square">X</button>;
+function Square({value}) {
+  function handleclick() {
+    console.log("clicked!");
+  }
+
+  return (  
+    <>
+      <button className="square2" onClick={handleclick}>{value}</button>
+    </>
+  )
+}
+export default function Board() {
+  return (
+    <>
+      <div className = "board">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  )
 }
