6c126f793c2de981d630d6443c5586e5b665b316
palak garg <gargpalak0401@gmail.com>
2025-03-11 02:41:40 +0530

    tic-tac-toe

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -13,7 +13,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleclick(i) {
-    if(squares[i]) return;
+    if(squares[i] || calculateWinner(squares)) return;
     const newSquares = squares.slice();
     if(xIsNext) newSquares[i] = "X";
     else newSquares[i] = "O";
@@ -21,8 +21,32 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for(let i=0; i<lines.length; i++){
+      const [a, b, c] = lines[i];
+      if(squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) return squares[a];
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) status =  "Winner: " + winner;
+  else status = "Next player: " + (xIsNext ? "X" : "O");
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className = "board">
         <Square value={ squares[0] } onSquareClick={ () => handleclick(0) } />
         <Square value={ squares[1] } onSquareClick={ () => handleclick(1) } />
