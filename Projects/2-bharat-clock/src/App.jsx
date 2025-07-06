6f18cb8047ad8f27c1e9aac1ea678bfdd77ffdff
palak garg <gargpalak0401@gmail.com>
2025-07-06 16:17:58 +0530

    bharat-clock

---
+++ Projects/2-bharat-clock/src/App.jsx
@@ -0,0 +1,15 @@
+import ClockHeading from "./components/ClockHeading"
+import ClockSlogan from "./components/ClockSlogan"
+import CurrentTime from "./components/CurrentTime"
+
+function App() {
+  return (
+    <center>
+      <ClockHeading />
+      <ClockSlogan />
+      <CurrentTime />
+    </center>
+  )
+}
+
+export default App
