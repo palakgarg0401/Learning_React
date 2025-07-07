869cb6c72549cadb8509bdb64aa16e8950a56b48
palak garg <gargpalak0401@gmail.com>
2025-07-07 20:40:07 +0530

    Fragments and Map method

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,16 +1,28 @@
 import Button from './Button.jsx'
 import Hello from './Hello.jsx';
 import Random from './Random.jsx';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {  //functionalf component
-  return <div>
+function App() {  //functional component
+
+  let fruitName = ['Apple', 'Mango', 'Grapes', 'Cherry', 'Litchi'];
+
+  //Fragments <> </>
+  return <>
     <h1>Hello World</h1>
     <Hello></Hello>
+    <br />
+    {/* Map Mathod */}
+    <h2>Fruits</h2>
+    <ul className="list-group">
+      {fruitName.map((item) => <li className="list-group-item">{item}</li>)}
+    </ul>
+    <br />
     <Button></Button>
     <Random></Random>
     <Random></Random>
     <Random></Random>
-  </div>
+  </>
 }
 
 export default App;
