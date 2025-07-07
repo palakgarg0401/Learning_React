6079ebf930a58a3ef507244a371d6fa0b1fbecc8
palak garg <gargpalak0401@gmail.com>
2025-07-07 20:56:38 +0530

    Conditional rendering

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -5,23 +5,37 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {  //functional component
 
-  let fruitName = ['Apple', 'Mango', 'Grapes', 'Cherry', 'Litchi'];
+  let fruitName = [];
+  //let fruitName = ['Apple', 'Mango', 'Grapes', 'Cherry', 'Litchi'];
+
+  // if(fruitName.length === 0){
+  //   return <h5>Fruit Basket is Empty!!</h5>
+  // }
+
+  //let emptyMsg = fruitName.length === 0 ? <h5>Fruit Basket is Empty</h5> : null;
 
   //Fragments <> </>
   return <>
     <h1>Hello World</h1>
     <Hello></Hello>
     <br />
+
     {/* Map Mathod */}
     <h2>Fruits</h2>
+
+    {/* {fruitName.length === 0 ? <h5>Fruit Basket is Empty</h5> : null}  //ternary */}
+    {/* {emptyMsg} */}
+    {fruitName.length === 0 && <h5>Fruit Basket is Empty</h5>}
+
     <ul className="list-group">
-      {fruitName.map((item) => <li className="list-group-item">{item}</li>)}
+      {fruitName.map((item) => <li key={item} className="list-group-item">{item}</li>)}
     </ul>
     <br />
-    <Button></Button>
+
+    {/* <Button></Button>
     <Random></Random>
     <Random></Random>
-    <Random></Random>
+    <Random></Random> */}
   </>
 }
 
