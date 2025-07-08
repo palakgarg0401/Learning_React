739b929ee2f6cdd8272f3028d6aeb7c7316f2c98
palak garg <gargpalak0401@gmail.com>
2025-07-08 14:30:39 +0530

    props

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,35 +1,23 @@
-import Button from './Button.jsx'
 import Hello from './Hello.jsx';
-import Random from './Random.jsx';
+//import Button from './Button.jsx'
+// import Random from './Random.jsx';
+import ErrorMessage from './components/ErrorMessage.jsx';
+import FoodItems from './components/FoodItems.jsx';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {  //functional component
 
-  let fruitName = [];
-  //let fruitName = ['Apple', 'Mango', 'Grapes', 'Cherry', 'Litchi'];
+    let foodItems = ['Dal', 'Green Vegetables', 'Roti', 'Salad', 'Milk'];
 
-  // if(fruitName.length === 0){
-  //   return <h5>Fruit Basket is Empty!!</h5>
-  // }
-
-  //let emptyMsg = fruitName.length === 0 ? <h5>Fruit Basket is Empty</h5> : null;
-
-  //Fragments <> </>
-  return <>
+    return <>
     <h1>Hello World</h1>
     <Hello></Hello>
     <br />
 
     {/* Map Mathod */}
-    <h2>Fruits</h2>
-
-    {/* {fruitName.length === 0 ? <h5>Fruit Basket is Empty</h5> : null}  //ternary */}
-    {/* {emptyMsg} */}
-    {fruitName.length === 0 && <h5>Fruit Basket is Empty</h5>}
-
-    <ul className="list-group">
-      {fruitName.map((item) => <li key={item} className="list-group-item">{item}</li>)}
-    </ul>
+    <h2>Healthy Food</h2>
+    <ErrorMessage items = {foodItems}></ErrorMessage>
+    <FoodItems items = {foodItems}></FoodItems>
     <br />
 
     {/* <Button></Button>
