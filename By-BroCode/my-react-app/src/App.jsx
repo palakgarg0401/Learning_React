508df412ba444a93392ff0301c52038544eb66a8
palak garg <gargpalak0401@gmail.com>
2025-07-18 10:47:06 +0530

    Learning by - BroCode (intro to react)

---
+++ By-BroCode/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Food from "./components/Food";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Food />
+      <Footer />
+    </>
+  );
+};
+
+export default App
